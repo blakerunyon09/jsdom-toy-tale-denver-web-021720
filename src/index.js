f51a1dd488ceb23b7bad2a34ffb39173ad2b1e95
Blake Runyon <blakerunyon09@gmail.com>
2021-03-23 09:30:24 -0600

    Crushed.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const toysAPI = 'http://localhost:3000/toys/'
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -13,3 +14,98 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetch(toysAPI)
+    .then(handleResponse)
+    .then(toys => {
+      toys.forEach( toy => {
+        createCard(toy)
+      })
+    })
+})
+
+const handleResponse = (response) => {
+  return response.json()
+}
+
+const createCard = (toy) => {
+  const toyContainer = document.querySelector('#toy-collection')
+  const div = document.createElement('div')
+  div.classList.add('card')
+
+  cardBody(toy, div)
+
+  toyContainer.append(div)
+}
+
+const cardBody = (toy, div) => {
+  const h2 = document.createElement('h2')
+  h2.textContent = toy.name
+
+  const img = document.createElement('img')
+  img.src = toy.image
+  img.classList.add('toy-avatar')
+
+  const p = document.createElement('p')
+  p.textContent = toy.likes
+
+  const btn = document.createElement('button')
+  btn.id = `toyid_${toy.id}`
+  btn.textContent = "Like"
+  btn.classList.add('like-btn')
+  btn.addEventListener('click', addLike)
+
+  div.append(h2, img, p, btn)
+}
+
+const addNewToyBtn = document.querySelector('input[type=submit]')
+addNewToyBtn.addEventListener('click', (e) => {
+  const form = document.querySelector('form')
+  e.preventDefault()
+
+  const formData = new FormData(form)
+  const toyName = formData.get('name')
+  const toyImage = formData.get('image')
+
+  toyOptions = {
+    name: toyName,
+    image: toyImage,
+    likes: 0
+  }
+
+  options = {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(toyOptions)
+  }
+  
+  fetch(toysAPI, options)
+    .then(handleResponse)
+    .then(createCard)
+})
+
+const addLike = (e) => {
+  const toyid = e.target.id.slice(-1)
+  let toyLikes = e.target.parentNode.querySelector('p')
+  let count = +toyLikes.textContent + 1
+
+  options ={
+    method: 'PATCH',
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      likes: count
+    })
+  }
+  
+  fetch(toysAPI + toyid, options)
+    .then(handleResponse)
+    .then(toyLikes.textContent = count)
+}
+
